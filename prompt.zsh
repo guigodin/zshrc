45beccc03af3da40fc369ccb117893cb4a6bbd62
ggodin <ggodin@nms-02.(none)>
2014-09-30 09:22:28 -0400

    fixed binding

--- prompt.zsh
+++ prompt.zsh
@@ -27,34 +27,34 @@ PROMPT='${at_bold}${fg_yellow}%m ${fg_red}%n ${fg_blue}%c ${at_normal}$(git_supe
 >> '
 # `
 
-# set_prompt_color() {
-#     if [[ $TERM = "linux" ]]; then
-#         # nothing
-#     elif [[ $TMUX != '' ]]; then
-#         printf '\033Ptmux;\033\033]12;%b\007\033\\' "$1"
-#     else
-#         echo -ne "\033]12;$1\007"
-#     fi
-# }
+set_prompt_color() {
+    if [[ $TERM = "linux" ]]; then
+        # nothing
+    elif [[ $TMUX != '' ]]; then
+        printf '\033Ptmux;\033\033]12;%b\007\033\\' "$1"
+    else
+        echo -ne "\033]12;$1\007"
+    fi
+}
 
-# # change cursor color basing on vi mode
-# zle-keymap-select () {
-#     if [ $KEYMAP = vicmd ]; then
-#         set_prompt_color $COMMAND_PROMPT
-#     else
-#         set_prompt_color $INSERT_PROMPT
-#     fi
-# }
+# change cursor color basing on vi mode
+zle-keymap-select () {
+    if [ $KEYMAP = vicmd ]; then
+        set_prompt_color $COMMAND_PROMPT
+    else
+        set_prompt_color $INSERT_PROMPT
+    fi
+}
 
-# zle-line-finish() {
-#     set_prompt_color $INSERT_PROMPT
-# }
+zle-line-finish() {
+    set_prompt_color $INSERT_PROMPT
+}
 
-# zle-line-init () {
-#     zle -K viins
-#     set_prompt_color $INSERT_PROMPT
-# }
+zle-line-init () {
+    zle -K viins
+    set_prompt_color $INSERT_PROMPT
+}
 
-# zle -N zle-keymap-select
-# zle -N zle-line-init
-# zle -N zle-line-finish
+zle -N zle-keymap-select
+zle -N zle-line-init
+zle -N zle-line-finish
